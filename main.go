97c7ac1b670c60d0f888e341aad72b9121fb6448
Hasari Tosun <hasari@gmail.coml>
2021-01-22 23:49:22 +0300

    change packages

--- main.go
+++ main.go
@@ -1,45 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand"
+	"github.com/codonex/towersampling/pkg/towersampling"
 )
 
-// Action Action object
-type Action struct {
-	Name        string
-	Probability float32
-}
-
-// Sampler samples data according to the probability
-//
-func Sampler(actions []Action) func() (string, error) {
-	probabilities := make([]float32, len(actions))
-	events := make([]string, len(actions))
-	for i, e := range actions {
-		if i == 0 {
-			probabilities[i] = e.Probability
-		} else {
-			probabilities[i] = e.Probability + probabilities[i-1]
-		}
-		events[i] = e.Name
-	}
-
-	return func() (string, error) {
-		value := rand.Float32()
-		for i, p := range probabilities {
-			if value <= p {
-				return events[i], nil
-			}
-		}
-		return "", errors.New("No action")
-	}
-
-}
-
 func main() {
-	sampler := Sampler([]Action{
+	sampler := towersampling.Sampler([]towersampling.Action{
 		{Name: "Sleep", Probability: 0.33},
 		{Name: "Walk", Probability: 0.05},
 		{Name: "Watch", Probability: 0.35},
---
+++ pkg/towersampling/tower.go
@@ -0,0 +1,38 @@
+package towersampling
+
+import (
+	"errors"
+	"math/rand"
+)
+
+// Action Action object
+type Action struct {
+	Name        string
+	Probability float32
+}
+
+// Sampler samples data according to the probability
+//
+func Sampler(actions []Action) func() (string, error) {
+	probabilities := make([]float32, len(actions))
+	events := make([]string, len(actions))
+	for i, e := range actions {
+		if i == 0 {
+			probabilities[i] = e.Probability
+		} else {
+			probabilities[i] = e.Probability + probabilities[i-1]
+		}
+		events[i] = e.Name
+	}
+
+	return func() (string, error) {
+		value := rand.Float32()
+		for i, p := range probabilities {
+			if value <= p {
+				return events[i], nil
+			}
+		}
+		return "", errors.New("No action")
+	}
+
+}
